77b590498a7e3e8d4ffed9a8a393e142dac957fb
Your Name <you@example.com>
2023-02-15 22:11:03 +0500

    added components directory with calculator.js file

---
+++ src/components/calculator.js
@@ -0,0 +1,26 @@
+const Calculator = () => {
+  return(
+    <p>0</p>
+    <p>AC</p>
+    <p>+/-</p>
+    <p>%</p>
+    <p>÷</p>
+    <p>7</p>
+    <p>8</p>
+    <p>9</p>
+    <p>x</p>
+    <p>4</p>
+    <p>5</p>
+    <p>6</p>
+    <p>-</p>
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+    <p>+</p>
+    <p>0</p>
+    <p>.</p>
+    <p>=</p>
+  );
+}
+
+export default Calculator;
