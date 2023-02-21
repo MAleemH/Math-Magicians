003ba623e7291d2c0c66b3ef26ce0d239ad9936c
MAleemH <aleemhashmi4321@gmail.com>
2023-02-21 22:21:04 +0500

    added quote and finalize styling of quote.js and navbar

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import LoremIpsum from 'react-lorem-ipsum';
 const Home = () => (
   <div className="homepage">
     <h2>Welcome to our page!</h2>
-    <p>
+    <p className="homepage-paragraphs">
       <LoremIpsum p={2} />
     </p>
   </div>
--- src/components/quote.js
+++ src/components/quote.js
@@ -1,5 +1,14 @@
 const Quote = () => (
-  <div>Quote Data</div>
+  <div className="quote-page">
+    <blockquote>
+      <q className="quote-description">
+        Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.
+      </q>
+      <p className="quote-author">
+      &mdash;William Paul Thurston
+      </p>
+    </blockquote>
+  </div>
 );
 
 export default Quote;
