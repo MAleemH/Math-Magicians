963bfdec02712b552a1cc62d5d83bc1e96b90f6d
MAleemH <aleemhashmi4321@gmail.com>
2023-02-21 21:43:40 +0500

    added calculator page with proper style

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,11 @@
 import LoremIpsum from 'react-lorem-ipsum';
 
 const Home = () => (
-  <div>
+  <div className="homepage">
     <h2>Welcome to our page!</h2>
-    <LoremIpsum p={2} />
+    <p>
+      <LoremIpsum p={2} />
+    </p>
   </div>
 );
 
